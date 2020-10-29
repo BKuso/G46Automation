5b69995a22b42b47ee62dbeb96b7fda756783671
kuso_bs <kuso@rozetka.com.ua>
2020-10-29 21:03:17 +0200

    refactoring

--- src/main/java/models/Record.java
+++ src/main/java/models/Record.java
@@ -1,9 +1,10 @@
-package additional;
+package models;
 
+import additional.Printer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public abstract class Record implements Printer{
+public abstract class Record implements Printer {
 
     private final static Logger LOG = LogManager.getLogger("Запись");
 
